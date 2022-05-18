8f0281433c10ed0a8d7df4dd762bff296f61d5ff
Joshua Morony <joshua.morony@gmail.com>
2022-05-18 20:20:21 +0930

    wip: add checklist in service

--- src/app/shared/data-access/checklist.service.spec.ts
+++ src/app/shared/data-access/checklist.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
 
 import { ChecklistService } from './checklist.service';
 
@@ -12,5 +13,23 @@ describe('ChecklistService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getChecklists()', () => {
+    it('should emit an empty array initially', () => {
+      const observerSpy = subscribeSpyTo(service.getChecklists());
+      expect(observerSpy.getLastValue()).toEqual([]);
+    });
+  });
+
+  describe('add()', () => {
+    it('should add to existing checklists and emit on checklists$', () => {
+      const testChecklist = { title: 'hello' };
+      const observerSpy = subscribeSpyTo(service.getChecklists());
+
+      service.add(testChecklist);
+
+      expect(observerSpy.getLastValue().includes(testChecklist)).toBe(true);
+    });
   });
 });
--- src/app/shared/data-access/checklist.service.ts
+++ src/app/shared/data-access/checklist.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Checklist } from '../interfaces/checklist';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChecklistService {
+  private checklists$ = new BehaviorSubject<Checklist[]>([]);
+
   constructor() {}
 
-  add(checklist: Checklist) {}
+  getChecklists() {
+    return this.checklists$.asObservable();
+  }
+
+  add(checklist: Checklist) {
+    this.checklists$.next([...this.checklists$.value, checklist]);
+  }
 }
